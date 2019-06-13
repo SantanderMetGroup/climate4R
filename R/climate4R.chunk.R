2d70bb134fecec8169752c79c3f5d4a472ed2e38
Your Name <maibide@gmail.com>
2019-06-13 11:47:25 +0200

    update and document climate4R.chunk

--- R/climate4R.chunk.R
+++ R/climate4R.chunk.R
@@ -17,44 +17,54 @@
 #     You should have received a copy of the GNU General Public License
 #     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-#' @title Apply climate4R function to each loaded set of latitudes.
+#' @title Apply climate4R function to each loaded set (chunk) of latitudes.
 #' @description Function tailored to operations performed over large data (e.g. for the entire world). 
 #' The function uses a loop where in each iteration a chunk of latitudes is loaded and a climate4R 
 #' function is applied to it. 
 #'
 #' @param n.chunks number of latitude chunks over which iterate
-#' @param C4R.FUN.args list of arguments passed to the selected C4R function (the name of the function 
-#' is the first element of the list). These arguments are passed to function \link{\code{do.call}}. 
-#' See Details.
+#' @param C4R.FUN.args list of arguments being the name of the C4R function (character)
+#' the first. The rest of the arguments are those passed to the selected C4R function. 
+#' This list is passed to function \link{\code{do.call}} internally. For the parameters 
+#' (of a particular C4R function) where data (grids) need to be provided, here, a list of 2 
+#' arguments are passed (instead of a grid): \code{list(dataset = "", var = "")}.
 #' @param loadGridData.args list of collocation arguments passed to function loadGridData.
 #' @param output.path Optional. Path where the results of each iteration will be saved (*.rda). 
-#' Usefull when the amount of data after the C4R function application is large, i.e. similar
+#' Useful when the amount of data after the C4R function application is large, i.e. similar
 #' to the pre-processed data (e.g. when the \link{\code{biasCorrection}} function is applied.)
-#' @details In \code{C4R.FUN.args}, for the parameters (of a particular C4R function) where 
-#' data (grids) need to be provided, here, a list of 2 arguments are passed: 
-#' \code{list(dataset = "", var = "")}.
-#' Note that the appropriate libraries need to be loaded before applying this function (loadeR, transformeR,
+#' @details Note that the appropriate libraries need to be loaded before applying this function. Packages
+#' \code{loadeR} and \code{transformeR} are always needed. Depending on the C4R function that 
+#' is applied the will also be needed to load the corresponding package/s.
 #' etc.)
 #' @return If \code{output.path} is NULL a grid containing all latitudes is returned. If \code{output.path}
-#' is provided *.rda objects for each latitude chunk are saved in the spedified path.
+#' is provided *.rda objects for each latitude chunk are saved in the specified path.
 #' @family climate4R
 #' 
 #'
 #' @author M. Iturbide
 #' @export
 #' @examples {
+#' source("R/climate4R.chunk.R")
+#' library(loadeR)
+#' library(transformeR)
+#' library(climate4R.climdex)
+#' 
+#' loginUDG(username = "", password = "")
+#' 
 #' z <- climate4R.chunk(n.chunks = 10,
-#'                      C4R.FUN.args = list(FUN = "climdexGrid", 
-#'                                          index.code = "CWD", 
+#'                      C4R.FUN.args = list(FUN = "climdexGrid",
+#'                                          index.code = "CWD",
 #'                                          pr = list(dataset = "WATCH_WFDEI", var = "pr")),
 #'                     loadGridData.args = list(years = 1981:2000,
 #'                                              lonLim = c(-10, 10),
 #'                                              latLim = c(36, 45)),
 #'                     output.path = NULL)
+#' library(visualizeR)
+#' spatialPlot(climatology(z))
 #'}
 
 climate4R.chunk <- function(n.chunks = 10,
-                            c4R.FUN.args = list(FUN = NULL),
+                            C4R.FUN.args = list(FUN = NULL),
                             loadGridData.args = list(),
                             output.path = NULL) {
   
@@ -63,8 +73,8 @@ climate4R.chunk <- function(n.chunks = 10,
     message("[", Sys.time(), "] Rdata will be saved in ", output.path)
   }
   #PREPARE DATA FOR loadGridData
-  ind.data <- which(unlist(lapply(c4R.FUN.args, function(w) "dataset" %in% names(w))))
-  data <- c4R.FUN.args[ind.data]
+  ind.data <- which(unlist(lapply(C4R.FUN.args, function(w) "dataset" %in% names(w))))
+  data <- C4R.FUN.args[ind.data]
   datasets <- lapply(data, '[[',"dataset")
   vars <- lapply(data, '[[',"var")
   df.datasets <- read.csv(file.path(find.package("loadeR"), "datasets.txt"), stringsAsFactors = FALSE)[ ,1:4]
@@ -109,22 +119,22 @@ climate4R.chunk <- function(n.chunks = 10,
     })
   })
   #LOOP FOR LOADING AND APPLYING THE C4R FUNCTION 
-  ind.FUN <- which(names(c4R.FUN.args) == "FUN")
-  if (length(ind.FUN) != 1) stop("FUN argument required in c4R.FUN.args")
-  message("[", Sys.time(), "] y contains ", n.lats.y[[1]], " latitudes. ", c4R.FUN.args[[ind.FUN]],  " will be applied in ", n.chunks, " chunks of about ", n.lat.chunk[[1]], " latitudes.")
+  ind.FUN <- which(names(C4R.FUN.args) == "FUN")
+  if (length(ind.FUN) != 1) stop("FUN argument required in C4R.FUN.args")
+  message("[", Sys.time(), "] y contains ", n.lats.y[[1]], " latitudes. ", C4R.FUN.args[[ind.FUN]],  " will be applied in ", n.chunks, " chunks of about ", n.lat.chunk[[1]], " latitudes.")
   out.ch <- lapply(1:length(lGD.args.chunk), function(i) {
     chunk.i <- lapply(lGD.args.chunk[[i]], function(d) do.call("loadGridData", d))
-    c4R.FUN.args[ind.data] <- chunk.i
-    c4R.chunk.output <- do.call(c4R.FUN.args[[ind.FUN]], c4R.FUN.args[-ind.FUN])
+    C4R.FUN.args[ind.data] <- chunk.i
+    C4R.chunk.output <- do.call(C4R.FUN.args[[ind.FUN]], C4R.FUN.args[-ind.FUN])
     if (!is.null(output.path)) {
       ni <- as.character(i)
       ndigits <-  nchar(length(lGD.args.chunk))
       if (nchar(i) < ndigits) ni <- paste0(paste0(rep("0", (ndigits - nchar(i))), collapse = ""), ni)
-      save(c4R.chunk.output, 
-           file = paste0(output.path, "/", paste(unlist(c4R.FUN.args[-ind.data]), collapse = "_"), "_chunk", ni, ".rda"))
-      paste0(output.path, "/", paste(unlist(c4R.FUN.args[-ind.data]), collapse = "_"), "_chunk_", ni, ".rda")
+      save(C4R.chunk.output, 
+           file = paste0(output.path, "/", paste(unlist(C4R.FUN.args[-ind.data]), collapse = "_"), "_chunk", ni, ".rda"))
+      paste0(output.path, "/", paste(unlist(C4R.FUN.args[-ind.data]), collapse = "_"), "_chunk_", ni, ".rda")
     } else {
-      c4R.chunk.output
+      C4R.chunk.output
     }
   })
   # RETURN
