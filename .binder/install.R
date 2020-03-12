cd7cb18a61cc4c07dc3171e7df7579d1a3de1ea2
Daniel Nüst <daniel.nuest@uni-muenster.de>
2020-03-12 17:07:43 +0100

    Create install.R

---
+++ .binder/install.R
@@ -0,0 +1,8 @@
+library("devtools")
+install_github(c("SantanderMetGroup/loadeR",
+                 "SantanderMetGroup/loadeR.java",
+                 "SantanderMetGroup/transformeR",
+                 "SantanderMetGroup/visualizeR",
+                 "SantanderMetGroup/downscaleR",
+                 "SantanderMetGroup/climate4R.climdex")
+ 
