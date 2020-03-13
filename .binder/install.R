6fa9bce19939e3b406c263853ccd0acd574f6ead
Daniel Nüst <daniel.nuest@uni-muenster.de>
2020-03-13 11:16:02 +0100

    add Java and fix install.R, add Binder badge

--- .binder/install.R
+++ .binder/install.R
@@ -1,8 +1,11 @@
+# from Rmd notebooks
 library("devtools")
-install_github(c("SantanderMetGroup/loadeR",
+install_github(c("SantanderMetGroup/climate4R.UDG",
                  "SantanderMetGroup/loadeR.java",
                  "SantanderMetGroup/transformeR",
                  "SantanderMetGroup/visualizeR",
                  "SantanderMetGroup/downscaleR",
-                 "SantanderMetGroup/climate4R.climdex")
- 
+                 "SantanderMetGroup/climate4R.climdex"))
+install_github(c("SantanderMetGroup/loadeR"))
+#
+install.packages("knitr")
